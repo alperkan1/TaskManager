00f87e8792634ed0cce9c62a725c2e28085f56fc
alperkan1 <seanhack@gmail.com>
2021-12-21 13:40:40 +0000

    add

--- static/js/script.js
+++ static/js/script.js
@@ -3,12 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
-    // Date Picker
-    let datepicker = document.querySelectorAll('.datepicker');
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
-        format: "dd mm, yyyy",
+        format: "dd mmmm, yyyy",
         i18n: {
             done: "Select"
         }
     });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
 });
